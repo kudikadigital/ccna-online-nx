e55ee98e2e7ef0e86b326b7236edcdf1d8f19135
Henriques Maculungungu <henriqueswaldemar1@gmail.com>
2026-01-16 12:20:31 +0100

    fix(db): resolve PrismaClient initialization error on Turbopack

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,7 +1,15 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
+import { PrismaClient } from "./generated/prisma";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+const adapter = new PrismaBetterSqlite3({
+  url: "file:./prisma/dev.db"
+})
+
+
+// Deixe o construtor vazio. O Prisma 6+ lerá o prisma.config.ts automaticamente
+export const prisma =
+  globalForPrisma.prisma || new PrismaClient({adapter});
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
